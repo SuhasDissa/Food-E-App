4aa82a3aaee2e434bbce3340de0236d07938dc4a
sebastien46 <57289288+sebastien46@users.noreply.github.com>
2024-02-11 03:00:15 +0100

    Use Static versions of Jetpack Libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,12 +77,12 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2024.02.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui:1.6.1")
+    implementation("androidx.compose.ui:ui-graphics:1.6.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.1")
+    implementation("androidx.compose.material3:material3:1.2.0")
 
-    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.6.1")
 
     implementation("androidx.compose.material3:material3-window-size-class:1.2.0")
 
